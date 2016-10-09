4a4dff56d4d23735b46f3ffdcb3ac441d3cd1a94
favianrahman <sfavianrahman@gmail.com>
2016-10-08 21:24:55 -0400

    Initial commit
    _functions.R taken from https://github.com/rajshah4/NBA_SportVu

---
+++ data_prep.R
@@ -0,0 +1,37 @@
+source("/Users/Favian/Box Sync/sportVU/_functions.R")
+
+# Getting SportVU data for the Spurs-Wolves game
+all.movements <- sportvu_convert_json(unzip("/Users/Favian/Box Sync/NBA-Defense-Analysis/0021500431.json.zip"))
+
+# Ordering the data by event ID, which seems to be a marker for a given play
+all.movements <- all.movements[order(all.movements$event.id),]
+
+# Since each event ID comprises of multiple "snapshots", we give each snapshot
+# a unique ID
+# When we join this table with play-by-play data below, this becomes important
+# so that we can conserve the order of every snapshot in the game
+all.movements$counter <- seq(1, length.out=nrow(all.movements), by=1)
+
+# Getting the play by play data for the game
+gameid = "0021500431"
+pbp <- get_pbp(gameid)
+
+# Joining the SportVU and play-by-play data by event ID
+pbp <- pbp[-1,]
+colnames(pbp)[2] <- c('event.id')
+#Trying to limit the fields to join to keep the overall size manageable
+pbp <- pbp %>% select (event.id,EVENTMSGTYPE,EVENTMSGACTIONTYPE,SCORE)
+pbp$event.id <- as.numeric(levels(pbp$event.id))[pbp$event.id]
+all.movements <- merge(x = all.movements, y = pbp, by = "event.id", all.x = TRUE)
+
+# The join mixes up the order of the plays
+# This is where the counter from before comes in handy
+# We just order by the counter to get the order we had before
+all.movements <- all.movements[order(all.movements$counter),]
+
+# Getting the distance matrix for the first event ID
+pickeventID <- 1
+dists_event1 <- player_dist_matrix(pickeventID)
+
+
+
